631d1eb55dc70672a6c597e9a709d9d8ce9378b2
rdavid1991 <rdavid3.1991@gmail.com>
2023-11-06 20:52:39 -0500

    Seccion 3 - Curso RUST

---
+++ section3/hello_world.rs
@@ -0,0 +1,10 @@
+/**
+ * La función main siempre se ejecuta
+ * es el punto de entrada para la aplicación
+ */
+fn main(){
+	/**
+	 * El signo 👉 ! es un macro de rust
+	 */
+	println!("Hola mundo")
+}
